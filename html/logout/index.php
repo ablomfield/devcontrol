116a33a2f67bea78ae4471ea89af4efd972e4c4f
Adam Blomfield <ablomfield@presidio.com>
2025-11-27 23:00:18 -0600

    logout

--- html/logout/index.php
+++ html/logout/index.php
@@ -6,5 +6,17 @@ include($_SERVER['DOCUMENT_ROOT'] . "/includes/settings.php");
 
 // Log Out
 session_destroy();
+
+// Remove Cookies
+if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+    }
+}
+
 header('Location: /');
 ?>
